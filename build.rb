12e41dfb2624765dc39c2f397c3346873f65c888
Pete Hodgson <git@thepete.net>
2012-08-19 10:07:01 -0700

    add a super-hacky build script

---
+++ build.rb
@@ -0,0 +1,21 @@
+require 'fileutils'
+
+def run_guard
+  Signal.trap("HUP") { exit }
+
+  require 'guard'
+  Guard.setup
+  Guard::Dsl.evaluate_guardfile(:guardfile => 'Guardfile')
+  Guard.start
+end
+
+guard_pid = fork { run_guard }
+
+sleep 2
+
+Dir['source/**/*'].each{ |f| FileUtils.touch(f) }
+
+sleep 2
+
+Process.kill "HUP", guard_pid
+Process.wait( guard_pid )
